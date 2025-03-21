7fb935b8e70a69b63e6932a360eca47d267bc090
ShreyasTupe <Shreyastupe299@gmail.com>
2025-03-21 09:31:44 +0530

    Update shubhexp2.py

--- shubhexp2.py
+++ shubhexp2.py
@@ -1,6 +1,6 @@
-x = int(input("Enter the first number: "))
-y = int(input("Enter the second number: "))
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
 
-z = x + y
+c = a + b
 
-print(f"The sum of {x} and {y} is {z}")
+print(f"The sum of {a} and {b} is {c}")
